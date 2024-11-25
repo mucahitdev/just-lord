8369cd28de6f675e23acb12439a870bad2234f52
Mücahit Kökdemir <76880536+mucahitdev@users.noreply.github.com>
2024-11-25 14:52:31 +0300

    iii

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,172 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Image, View, TouchableOpacity, Text } from "react-native";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+import { useCallback, useEffect, useRef } from "react";
+import { Swiper, SwiperCardRefType } from "rn-swiper-list";
+import React from "react";
+import { AntDesign } from "@expo/vector-icons";
+import { data } from "@/constants/data";
+
+type ActionButtonProps = TouchableOpacity["props"] & {
+  onTap?: () => void;
+};
+
+// eslint-disable-next-line react/display-name
+export const ActionButton = React.memo(
+  ({ onTap, style, children, ...rest }: ActionButtonProps) => {
+    return (
+      <TouchableOpacity onPress={onTap} {...rest} style={style}>
+        {children}
+      </TouchableOpacity>
+    );
+    // eslint-disable-next-line prettier/prettier
+  }
+);
 
 export default function TabOneScreen() {
+  const ref = useRef<SwiperCardRefType>();
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+
+  const [cardData, setCardData] = React.useState(data);
+
+  useEffect(() => {
+    setCardData(data);
+  }, []);
+
+  const RenderCard = useCallback(
+    ({
+      index,
+      question,
+      labelLeft,
+    }: {
+      index: number;
+      question: string;
+      labelLeft: string;
+    }) => {
+      const isActive = currentIndex === index;
+      return (
+        <View style={[styles.renderCardContainer]}>
+          {isActive ? (
+            <View style={{ backgroundColor: "pink", flex: 1 }}>
+              <Text>{question}</Text>
+            </View>
+          ) : (
+            <View style={styles.renderCardImage}>
+              <Text>Sıradaki</Text>
+            </View>
+          )}
+
+          <Text>{labelLeft}</Text>
+        </View>
+      );
+    },
+    [currentIndex]
+  );
+  const OverlayLabelRight = useCallback(() => {
+    return (
+      <View
+        style={[
+          styles.overlayLabelContainer,
+          {
+            backgroundColor: "green",
+          },
+        ]}
+      >
+        <Text style={{ fontWeight: "700", fontSize: 32 }}>
+          {data[currentIndex]?.labelRight}
+        </Text>
+      </View>
+    );
+  }, [currentIndex]);
+  const OverlayLabelLeft = useCallback(() => {
+    return (
+      <View
+        style={[
+          styles.overlayLabelContainer,
+          {
+            backgroundColor: "red",
+          },
+        ]}
+      >
+        <Text style={{ fontWeight: "700", fontSize: 32 }}>
+          {data[currentIndex]?.labelLeft}
+        </Text>
+      </View>
+    );
+  }, [currentIndex]);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View style={styles.subContainer}>
+        <Swiper
+          ref={ref}
+          cardStyle={styles.cardStyle}
+          data={data}
+          renderCard={(item, i) => {
+            console.log("🚀 ~ TabOneScreen ~ i:", i);
+            return <RenderCard {...item} index={i} />;
+          }}
+          onSwipeRight={(cardIndex) => {
+            console.log("cardIndex", cardIndex);
+            setCurrentIndex(cardIndex + 1);
+          }}
+          onSwipedAll={() => {
+            console.log("onSwipedAll");
+          }}
+          onSwipeLeft={(cardIndex) => {
+            console.log("onSwipeLeft", cardIndex);
+            setCurrentIndex(cardIndex + 1);
+          }}
+          disableTopSwipe
+          onSwipeTop={(cardIndex) => {
+            console.log("onSwipeTop", cardIndex);
+          }}
+          OverlayLabelRight={OverlayLabelRight}
+          OverlayLabelLeft={OverlayLabelLeft}
+          onSwipeActive={() => {
+            console.log("onSwipeActive");
+          }}
+          onSwipeStart={() => {
+            console.log("onSwipeStart");
+          }}
+          onSwipeEnd={() => {
+            console.log("onSwipeEnd");
+          }}
+        />
+      </View>
+
+      {/* <View style={styles.buttonsContainer}>
+        <ActionButton
+          style={styles.button}
+          onTap={() => {
+            ref.current?.swipeLeft();
+          }}
+        >
+          <AntDesign name="close" size={32} color="white" />
+        </ActionButton>
+        <ActionButton
+          style={[styles.button, { height: 60, marginHorizontal: 10 }]}
+          onTap={() => {
+            ref.current?.swipeBack();
+          }}
+        >
+          <AntDesign name="reload1" size={24} color="white" />
+        </ActionButton>
+        <ActionButton
+          style={styles.button}
+          onTap={() => {
+            ref.current?.swipeTop();
+          }}
+        >
+          <AntDesign name="arrowup" size={32} color="white" />
+        </ActionButton>
+        <ActionButton
+          style={styles.button}
+          onTap={() => {
+            ref.current?.swipeRight();
+          }}
+        >
+          <AntDesign name="heart" size={32} color="white" />
+        </ActionButton>
+      </View> */}
     </View>
   );
 }
@@ -20,16 +174,62 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    bottom: 34,
     alignItems: "center",
     justifyContent: "center",
   },
-  title: {
+  button: {
+    height: 80,
+    borderRadius: 40,
+    marginHorizontal: 20,
+    aspectRatio: 1,
+    backgroundColor: "#3A3D45",
+    elevation: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "black",
+    shadowOpacity: 0.1,
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+  },
+  buttonText: {
     fontSize: 20,
     fontWeight: "bold",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  cardStyle: {
+    width: "95%",
+    height: "75%",
+    borderRadius: 15,
+    marginVertical: 20,
+  },
+  renderCardContainer: {
+    flex: 1,
+    borderRadius: 15,
+    height: "75%",
+    width: "100%",
+  },
+  renderCardImage: {
+    height: "100%",
+    width: "100%",
+    borderRadius: 15,
+    backgroundColor: "gray",
+  },
+  subContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  overlayLabelContainer: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ constants/data.ts
@@ -0,0 +1,56 @@
+export const data = [
+  {
+    id: 1,
+    labelLeft: "Vemeyelim",
+    labelRight: "Verelim",
+    question: "Vermelimi vermemelimi?",
+  },
+  {
+    id: 2,
+    labelLeft: "Sorma",
+    labelRight: "Sor",
+    question: "Sormalımı Sormalımı?",
+  },
+  {
+    id: 3,
+    labelLeft: "Git",
+    labelRight: "Gel",
+    question: "Sormalımı Sormalımı?",
+  },
+  {
+    id: 4,
+    labelLeft: "Git",
+    labelRight: "Gel",
+    question: "Sormalımı Sormalımı?",
+  },
+  {
+    id: 5,
+    labelLeft: "Sev",
+    labelRight: "Sevme",
+    question: "Sormalımı Sormalımı?",
+  },
+  {
+    id: 6,
+    labelLeft: "aşk",
+    labelRight: "nefret",
+    question: "Sormalımı Sormalımı?",
+  },
+  {
+    id: 7,
+    labelLeft: "korks",
+    labelRight: "sorkd",
+    question: "Sormalımı Sormalımı?",
+  },
+  {
+    id: 8,
+    labelLeft: "lefti",
+    labelRight: "regti",
+    question: "Sormalımı Sormalımı?",
+  },
+  {
+    id: 9,
+    labelLeft: "dokizki",
+    labelRight: "elki",
+    question: "Sormalımı Sormalımı?",
+  },
+];
